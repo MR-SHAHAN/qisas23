54fe5a273855a3e8cc0f17da849f2befa2ed2d86
MR-SHAHAN <155003337+MR-SHAHAN@users.noreply.github.com>
2023-12-27 22:10:09 +0530

    Add files via upload
    d

---
+++ others/input/search.js
@@ -0,0 +1,287 @@
+let popup = document.getElementById("popup");
+let rePopup = document.getElementById("link-popup");
+let inputBox = document.getElementById("input");
+let result = document.getElementById("mem-btn");
+let link = document.getElementById("mem-link");
+
+function openPopup(){
+    popup.style.left = `78%`;
+}
+
+function closePopup(){
+    popup.style.left = `100%`;
+}
+
+function openResult(){
+    rePopup.style.left = `78%`;
+}
+
+function closeResult(){
+    rePopup.style.left = `100%`;
+}
+
+function showResult(){
+    
+    if (inputBox.value === ''){
+        alert("You must write your Chest-no");
+        rePopup.style.left = `100%`;
+    }
+    if (inputBox.value === '304'){
+        result.innerHTML = "Muhammed Shahan K";
+        link.href = `/members/shahan/shahan.html`;
+    }
+    if (inputBox.value === '212'){
+        result.innerHTML = "Muhammed Fauzan M";
+        link.href = `/members/fauzan/fauzan.html`;
+    }
+
+    if (inputBox.value === '101'){
+        result.innerHTML = "Muhammed Irfan kp";
+        link.href = `/members/irfan kp/irfan kp.html`;
+    }
+
+    if (inputBox.value === '102'){
+        result.innerHTML = "Muhammed Arshad mp";
+        link.href = `/members/arshad/arshad.html`;
+    }
+
+    if (inputBox.value === '103'){
+        result.innerHTML = "Muhammed Sabiq p";
+        link.href = `/members/sabiq/sabiq.html`;
+    }
+    if (inputBox.value === '104'){
+        result.innerHTML = "Muhammed ";
+        link.href = `/members/.html`;
+    }
+
+    if (inputBox.value === '216'){
+        result.innerHTML = "Muhammed Irfan KM";
+        link.href = `/members/irfan km/irfanm km.html`;
+    }
+    if (inputBox.value === '104'){
+        result.innerHTML = "Muhammed Ziyad E";
+        link.href = `/members/ziyad/ziyad.html`;
+    }if (inputBox.value === '105'){
+        result.innerHTML = "Muhammed Sabith PK";
+        link.href = `/members/sabith/sabith.html`;
+    }
+    if (inputBox.value === '106'){
+        result.innerHTML = "Musthafa Muhammed O";
+        link.href = `/members/musthafa/musthafa.html`;
+    }
+    if (inputBox.value === '107'){
+        result.innerHTML = "Shameemul Hasan TK ";
+        link.href = `/members/hasan/hasan.html`;
+    }
+    if (inputBox.value === '108'){
+        result.innerHTML = "Muhammed Anas T";
+        link.href = `/members/anas/anas.html`;
+    }
+    if (inputBox.value === '109'){
+        result.innerHTML = "Muhammed Mujthaba KK";
+        link.href = `/members/mujthaba/mujthaba.html`;
+    }
+    if (inputBox.value === '110'){
+        result.innerHTML = "Shiblu Rahman MT";
+        link.href = `/members/shiblu/shiblu.html`;
+    }
+    if (inputBox.value === '111'){
+        result.innerHTML = "Yaseen Bishr K";
+        link.href = `/members/yaseen/yaseen.html`;
+    }
+    if (inputBox.value === '112'){
+        result.innerHTML = "Muhammed Hanan a";
+        link.href = `/members/hanan a/hanan a.html`;
+    }
+    if (inputBox.value === '113'){
+        result.innerHTML = "Muhammed Munavvar pp";
+        link.href = `/members/munavvar/munavvar.html`;
+    }
+    if (inputBox.value === '114'){
+        result.innerHTML = "Muhammed Jinan p";
+        link.href = `/members/jinan/jinan.html`;
+    }
+    if (inputBox.value === '115'){
+        result.innerHTML = "Muhammed Shammas ck";
+        link.href = `/members/shammas/shammas.html`;
+    }
+    if (inputBox.value === '116'){
+        result.innerHTML = "Muhammed Sinan c";
+        link.href = `/members/sinan/sinan.html`;
+    }
+    if (inputBox.value === '117'){
+        result.innerHTML = "Muhammed Nihal ma";
+        link.href = `/members/nihal ma/nihal ma.html`;
+    }
+    
+    if (inputBox.value === '201'){
+        result.innerHTML = "Ahmed Rabeeh u";
+        link.href = `/members/rabeeh u/rabeeh u.html`;
+    }
+    
+    if (inputBox.value === '202'){
+        result.innerHTML = "Muhammed Adil mt";
+        link.href = `/members/adil/adil.html`;
+    }
+    
+    if (inputBox.value === '203'){
+        result.innerHTML = "Fahmudheen a";
+        link.href = `/members/fahmudheen/fahmudheen.html`;
+    }
+    
+    if (inputBox.value === '204'){
+        result.innerHTML = "wafi Musthafa rk";
+        link.href = `/members/wafi/wafi.html`;
+    }
+    
+    if (inputBox.value === '205'){
+        result.innerHTML = "Muhammed Murshid p";
+        link.href = `/members/murshid/murshid.html`;
+    }
+    
+    if (inputBox.value === '206'){
+        result.innerHTML = "Muhammed thoyyib";
+        link.href = `/members/thoyyib/thoyyib.html`;
+    }
+    
+    if (inputBox.value === '207'){
+        result.innerHTML = "Muhammed Nasim k";
+        link.href = `/members/nasim/nasim.html`;
+    }
+    
+    if (inputBox.value === '208'){
+        result.innerHTML = "Ahmed Shaheer ek";
+        link.href = `/members/shaheer/shaheer.html`;
+    }
+    
+    if (inputBox.value === '209'){
+        result.innerHTML = "Rabeeh Ahmed ct";
+        link.href = `/members/rabeeh ct/rabeeh ct.html`;
+    }
+    
+    if (inputBox.value === '210'){
+        result.innerHTML = "Abshar Khan AK";
+        link.href = `/members/abshar/abshar.html`;
+    }
+    
+    if (inputBox.value === '211'){
+        result.innerHTML = "Muhammed Ajmal ok";
+        link.href = `/members/ajmal/ajmal.html`;
+    }
+    
+    if (inputBox.value === '213'){
+        result.innerHTML = "Muhammed Nafi'";
+        link.href = `/members/nafi'/nafi'.html`;
+    }
+    
+    if (inputBox.value === '214'){
+        result.innerHTML = "Muhammed Niyas";
+        link.href = `/members/niyas/niyas.html`;
+    }
+    
+    if (inputBox.value === '215'){
+        result.innerHTML = "Muhammed Uvais";
+        link.href = `/members/uvais/uvais.html`;
+    }
+    
+    if (inputBox.value === '216'){
+        result.innerHTML = "Muhammed Irfan km";
+        link.href = `/members/irfan km/irfan km.html`;
+    }
+
+    if (inputBox.value === '217'){
+        result.innerHTML = "Muhammed Nihal n";
+        link.href = `/members/nihal n/nihal n.html`;
+    }
+    
+    if (inputBox.value === '218'){
+        result.innerHTML = "Ibrahim Bhadusha";
+        link.href = `/members/hanan a/bhadusha.html`;
+    }
+    
+    if (inputBox.value === '301'){
+        result.innerHTML = "Muhammed Faheem vm ";
+        link.href = `/members/faheem/faheem.html`;
+    }
+    
+    if (inputBox.value === '302'){
+        result.innerHTML = "Muhammed Jalal";
+        link.href = `/members/jalal/jalal.html`;
+    }
+    
+    if (inputBox.value === '303'){
+        result.innerHTML = "Ubaidullah v";
+        link.href = `/members/ubaidullah/ubaidullah.html`;
+    }
+    
+    if (inputBox.value === '304'){
+        result.innerHTML = "Muhammed Shahan k";
+        link.href = `/members/shahan/shahan.html`;
+    }
+    
+    if (inputBox.value === '305'){
+        result.innerHTML = "Muhammed Safwan kk";
+        link.href = `/members/safwan/safwan.html`;
+    }
+    
+    if (inputBox.value === '306'){
+        result.innerHTML = "Muhammed Anshif PP";
+        link.href = `/members/anshif/anshif.html`;
+    }
+    
+    if (inputBox.value === '307'){
+        result.innerHTML = "Muhammed Ashmil K";
+        link.href = `/members/ashmil/ashmil.html`;
+    }
+    
+    if (inputBox.value === '308'){
+        result.innerHTML = "Muhammed Shameem M";
+        link.href = `/members/shameem/shameem.html`;
+    }
+    
+    if (inputBox.value === '309'){
+        result.innerHTML = "Muhammed Juman I";
+        link.href = `/members/juman/juman.html`;
+    }
+    
+    if (inputBox.value === '310'){
+        result.innerHTML = "Sayyid Sahal SV";
+        link.href = `/members/sahal/sahal.html`;
+    }
+    
+    if (inputBox.value === '311'){
+        result.innerHTML = "Muhammed Nihad K";
+        link.href = `/members/nihad/nihad.html`;
+    }
+
+    if (inputBox.value === '312'){
+        result.innerHTML = "Jesil Inthisham PK";
+        link.href = `/members/jesil/jesil.html`;
+    }
+
+    if (inputBox.value === '313'){
+        result.innerHTML = "Muhammed Shamal T";
+        link.href = `/members/shamal/shamal.html`;
+    }
+
+    if (inputBox.value === '314'){
+        result.innerHTML = "Muhammed Raif TP";
+        link.href = `/members/raif/raif.html`;
+    }
+
+    if (inputBox.value === '315'){
+        result.innerHTML = "Hashim Husain P";
+        link.href = `/members/hashim/hashim.html`;
+    }if (inputBox.value === '316'){
+        result.innerHTML = "Muhammed Bishr KV";
+        link.href = `/members/bishr/bishr.html`;
+    }if (inputBox.value === '317'){
+        result.innerHTML = "Muhammed Shamil T";
+        link.href = `/members/shamil t/shamil t.html`;
+    }
+    
+    if (inputBox.value === '318'){
+        result.innerHTML = "Muhammed Afnan MP";
+        link.href = `/members/afnan/afnan.html`;
+    }
+}
